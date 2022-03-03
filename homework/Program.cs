30fcff502f8cfc861edaf4b0eca8f1587f9a1478
Jay <ababqw123@gmail.com>
2022-03-03 09:44:02 +0900

    초기 커밋.

---
+++ homework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int result=1;
+
+            for(int i = a; i > 0; i--)
+            {
+                result *= i;
+                
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
