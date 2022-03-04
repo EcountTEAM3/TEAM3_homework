b901da5392df5d6015d030d27fd9dd00b2c2724f
ababqw123 <96455920+ababqw123@users.noreply.github.com>
2022-03-04 17:48:25 +0900

    education

---
+++ Lotto/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lotto
+{
+
+     class Lotto
+    {
+        int[] number;
+        int[] array;
+        Random random;
+
+        public Lotto()
+        {
+            random = new Random();
+            int y;
+
+        }
+
+        public void getnumber()
+        {
+            int[] num = new int[6];
+            Random rand = new Random();
+            int temp;
+
+            for (int i = 0; i < 6; i++)
+            {
+                int a = rand.Next(1, 45);
+                num[i] = a;
+                for (int j = 0; j < i; j++)
+                {
+                    if (i == 0) break;
+                    num[i] = (num[i] == num[j]) ? i-- : num[i];
+                }
+            }
+            for (int i = 0; i < num.Length-1; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if(num[i] > num[j])
+                    {
+                        temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+                    }
+                }
+            }
+
+            foreach (int num1 in num)
+            {
+                Console.WriteLine(num1);
+            }
+
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lotto lotto = new Lotto();
+            lotto.getnumber();
+            
+           
+            
+
+
+        }
+    }
+}
