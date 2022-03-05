34343ea9f28cb10553e599790adfbdd493f7406b
Jay <ababqw123@gmail.com>
2022-03-05 13:28:51 +0900

    fisrt

---
+++ Cinema_team/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cinema_team;
+
+namespace Cinema_team
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cinema cinema = new Cinema();
+            cinema.mainmenu();
+        }
+    }
+}
